20a8fc00eae3e778b2a53ed2ed102257940491e5
Your Name <you@example.com>
2025-04-02 17:02:20 +0000

    adding cros

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Listen on all interfaces (required for Docker)
-    port: 5174,      // Match the port in your Docker Compose file
+    host: '0.0.0.0', 
+    port: 5174,      
     allowedHosts: [
       'dashboard.d2f.io.vn', // Allow this hostname
       'localhost',           // Optional: keep for local development
